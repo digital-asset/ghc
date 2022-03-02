f18603c12c11806b23a2c450d3b155c06555f4a0
Moritz Kiefer <moritz.kiefer@purelyfunctional.org>
2022-03-02 17:37:37 +0100

    Add a nix setup for the build (#109)
    * Add a nix setup for the build
    
    fixes https://github.com/digital-asset/daml/issues/13081

---
+++ shell.nix
@@ -0,0 +1,24 @@
+let
+  nixpkgs = builtins.fetchTarball {
+    url = https://github.com/NixOS/nixpkgs/archive/22dc22f8cedc58fcb11afe1acb08e9999e78be9c.tar.gz;
+    sha256 = "0bkkc0054ayk41xhm3k39lgy9riljxk9l80p68afycadj0czvmwz";
+  };
+  pkgs = import nixpkgs {};
+in
+pkgs.mkShell {
+  buildInputs = with pkgs; [
+    autoconf
+    automake
+    cabal-install
+    libiconv
+    gmp
+    git
+    haskell.packages.ghc865Binary.ghc
+    haskellPackages.alex
+    haskellPackages.happy_1_19_12
+    m4
+    ncurses
+    perl
+    python3
+  ];
+}
