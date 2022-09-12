4f60ec3ea93c086c7c5d5725299c477974e7725f
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2022-09-12 17:27:14 +0100

    Clean up customDamlErrors, check for multiple possible importing modules

--- compiler/typecheck/TcDaml.hs
+++ compiler/typecheck/TcDaml.hs
@@ -14,27 +14,33 @@ import Name
 import Module
 import PrelNames
 
+check :: [String] -> String -> TyCon -> Bool
+check modules name con
+  | Just mod <- moduleNameString . moduleName <$> nameModule_maybe (tyConName con)
+  , mod `elem` modules
+  , name == occNameString (occName (tyConName con))
+  = True
+  | otherwise
+  = False
+
 customDamlErrors :: Ct -> [ClsInst] -> SDoc -> Maybe SDoc
 customDamlErrors ct candidate_insts binds_msg
-  | TyConApp con [TyConApp target [], viewType] <- ctev_pred (ctEvidence ct)
   -- TODO: Use PrelNames machinery to match unique names instead of resorting to string matching
-  , Just "DA.Internal.Desugar" <- moduleNameString . moduleName <$> nameModule_maybe (tyConName con)
-  , "HasInterfaceView" <- occNameString $ occName $ tyConName con
+  | TyConApp con [TyConApp target [], viewType] <- ctev_pred (ctEvidence ct)
+  , check ["DA.Internal.Desugar", "DA.Internal.Interface"] "HasInterfaceView" con
   = Just
   $ vcat [ text "Tried to get an interface view of type" <+> ppr viewType <+> text "from a non-interface" <+> ppr (tyConName target)
          , text "If" <+> ppr (tyConName target) <+> text "is a template, try casting it using toInterface or toInterfaceContractId"
          ]
   | TyConApp con [TyConApp target [], TyConApp choiceName [], result] <- ctev_pred (ctEvidence ct)
-  , Just "DA.Internal.Desugar" <- moduleNameString . moduleName <$> nameModule_maybe (tyConName con)
-  , "HasExercise" <- occNameString $ occName $ tyConName con
+  , check ["DA.Internal.Desugar", "DA.Internal.Template.Functions"] "HasExercise" con
   = Just
   $ vcat [ text "Tried to exercise a choice" <+> ppr choiceName <+> text "which doesn't exist on" <+> ppr (tyConName target)
          , text "If the choice" <+> ppr choiceName <+> text "belongs to an interface, try casting" <+> ppr (tyConName target) <+> text "using toInterface or toInterfaceContractId"
          ]
   | TyConApp con [TyConApp target [], LitTy (StrTyLit methodName), result] <- ctev_pred (ctEvidence ct)
-  , Just "DA.Internal.Desugar" <- moduleNameString . moduleName <$> nameModule_maybe (tyConName con)
-  , "HasMethod" <- occNameString $ occName $ tyConName con
+  , check ["DA.Internal.Desugar"] "HasMethod" con
   = Just
   $ text "Tried to implement method" <+> ppr methodName <> text ", but interface" <+> ppr (tyConName target) <+> text "does not have a method with that name."
-  | otherwise = Nothing
-
+  | otherwise
+  = Nothing
