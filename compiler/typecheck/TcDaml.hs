aad7c030a29a09b414f49fc50d86d73b171f33ec
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2022-09-29 12:52:17 +0100

    Make TcDaml trivially effectful for now

--- compiler/typecheck/TcDaml.hs
+++ compiler/typecheck/TcDaml.hs
@@ -23,24 +23,24 @@ check modules name con
   | otherwise
   = False
 
-customDamlErrors :: Ct -> [ClsInst] -> SDoc -> Maybe SDoc
-customDamlErrors ct candidate_insts binds_msg
+customDamlErrors :: Ct -> TcM (Maybe SDoc)
+customDamlErrors ct
   -- TODO: Use PrelNames machinery to match unique names instead of resorting to string matching
   | TyConApp con [TyConApp target [], viewType] <- ctev_pred (ctEvidence ct)
   , check ["DA.Internal.Desugar", "DA.Internal.Interface"] "HasInterfaceView" con
-  = Just
+  = pure $ Just
   $ vcat [ text "Tried to get an interface view of type" <+> ppr viewType <+> text "from a non-interface" <+> ppr (tyConName target)
          , text "If" <+> ppr (tyConName target) <+> text "is a template, try casting it using toInterface or toInterfaceContractId"
          ]
   | TyConApp con [TyConApp target [], TyConApp choiceName [], result] <- ctev_pred (ctEvidence ct)
   , check ["DA.Internal.Desugar", "DA.Internal.Template.Functions"] "HasExercise" con
-  = Just
+  = pure $ Just
   $ vcat [ text "Tried to exercise a choice" <+> ppr choiceName <+> text "which doesn't exist on" <+> ppr (tyConName target)
          , text "If the choice" <+> ppr choiceName <+> text "belongs to an interface, try casting" <+> ppr (tyConName target) <+> text "using toInterface or toInterfaceContractId"
          ]
   | TyConApp con [TyConApp target [], LitTy (StrTyLit methodName), result] <- ctev_pred (ctEvidence ct)
   , check ["DA.Internal.Desugar"] "HasMethod" con
-  = Just
+  = pure $ Just
   $ text "Tried to implement method" <+> ppr methodName <> text ", but interface" <+> ppr (tyConName target) <+> text "does not have a method with that name."
   | otherwise
-  = Nothing
+  = pure Nothing
