85b7984fb30684d71eb87099d512ae7ba6f5828b
Dylan Thinnes <dylan.thinnes@digitalasset.com>
2022-09-30 12:53:24 +0100

    Split customDamlErrors logic to customDamlErrorsPure

--- compiler/typecheck/TcDaml.hs
+++ compiler/typecheck/TcDaml.hs
@@ -19,6 +19,7 @@ import HsDecls
 import HsExtension
 import Type
 import FastString
+import TcRnMonad
 
 import Data.Maybe
 import Data.List
@@ -33,26 +34,29 @@ check modules name namedThing
   = False
 
 customDamlErrors :: Ct -> TcM (Maybe SDoc)
-customDamlErrors ct
+customDamlErrors ct = customDamlErrorsPure <$> getGblEnv <*> pure ct
+
+customDamlErrorsPure :: TcGblEnv -> Ct -> Maybe SDoc
+customDamlErrorsPure env ct
   -- TODO: Use PrelNames machinery to match unique names instead of resorting to string matching
   | TyConApp con [TyConApp target [], viewType] <- ctev_pred (ctEvidence ct)
   , check ["DA.Internal.Desugar", "DA.Internal.Interface"] "HasInterfaceView" con
-  = pure $ Just
+  = Just
   $ vcat [ text "Tried to get an interface view of type" <+> ppr viewType <+> text "from a non-interface" <+> ppr (tyConName target)
          , text "If" <+> ppr (tyConName target) <+> text "is a template, try casting it using toInterface or toInterfaceContractId"
          ]
   | TyConApp con [TyConApp target [], TyConApp choiceName [], result] <- ctev_pred (ctEvidence ct)
   , check ["DA.Internal.Desugar", "DA.Internal.Template.Functions"] "HasExercise" con
-  = pure $ Just
+  = Just
   $ vcat [ text "Tried to exercise a choice" <+> ppr choiceName <+> text "which doesn't exist on" <+> ppr (tyConName target)
          , text "If the choice" <+> ppr choiceName <+> text "belongs to an interface, try casting" <+> ppr (tyConName target) <+> text "using toInterface or toInterfaceContractId"
          ]
   | TyConApp con [TyConApp target [], LitTy (StrTyLit methodName), result] <- ctev_pred (ctEvidence ct)
   , check ["DA.Internal.Desugar"] "HasMethod" con
-  = pure $ Just
+  = Just
   $ text "Tried to implement method" <+> ppr methodName <> text ", but interface" <+> ppr (tyConName target) <+> text "does not have a method with that name."
   | otherwise
-  = pure Nothing
+  = Nothing
 
 data DamlVariant = Template Name | Interface Name | Choice Name
 
