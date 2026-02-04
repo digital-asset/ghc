6c1f036ca835d6168099452e470dba838c77941c
Jasper Van der Jeugt <jasper.vanderjeugt@digitalasset.com>
2026-02-04 12:07:10 +0100

    Clean up TcGenDamlSerializable

--- compiler/typecheck/TcGenDamlSerializable.hs
+++ compiler/typecheck/TcGenDamlSerializable.hs
@@ -1,6 +1,7 @@
--- This generates binds when using "deriving (Serializable)" in DAML.
--- It is mostly similar to gen_Show_binds in TcGenDeriv.hs.
--- I split it into a separate module to make future merges (hopefully) easier.
+-- NOTE(jaspervdj): This generates binds when using "deriving (Serializable)" in
+-- DAML.  When porting or upgrading this, I would recommend looking at the
+-- structure of gen_Show_binds in TcGenDeriv.hs.  I split it into a separate
+-- module to make future merges (hopefully) easier.
 {-# LANGUAGE GADTs #-}
 module TcGenDamlSerializable
     ( gen_Serializable_binds
@@ -22,40 +23,31 @@ import TyCon
 gen_Serializable_binds :: SrcSpan -> TyCon
                        -> (LHsBinds GhcPs, BagDerivStuff)
 
-gen_Serializable_binds loc tycon
-  = (unitBag witness_bind, emptyBag)
+gen_Serializable_binds loc tycon = (unitBag witness_bind, emptyBag)
   where
-    witness_rdr = damlSerializableWitness_RDR
-    data_cons = tyConDataCons tycon
-    -- witness_bind = mkFunBindEC 1 loc damlSerializableWitness_RDR id (map pats_etc data_cons)
+    witness_arity = 2
 
-    witness_bind = mkRdrFunBindEC 1 id (L loc witness_rdr)
-        [ mkMatch (mkPrefixFunRhs (L loc witness_rdr))
-                                (map (parenthesizePat appPrec) p) e
-                                (noLoc emptyLocalBinds)
-        | (p,e) <- map pats_etc data_cons
-        ]
+    witness_bind = mkRdrFunBindEC
+        witness_arity
+        id
+        (L loc damlSerializableWitness_RDR)
+        (map witness_match (tyConDataCons tycon))
 
-    {-
-    pats_etc data_con =
-     ([con_pat], nlHsApp
-         (nlHsVar (getRdrName mconcatName))
-         (nlList $ map (nlHsApp witness . nlHsVar) as_needed))
-         -}
-
-    pats_etc data_con =
-         ([con_pat, nlVarPat z_rdr], foldr
-             (\v expr -> (nlHsApp (nlHsApp witness (nlHsVar v)) expr))
-             (nlHsVar z_rdr)
-             as_needed)
+    witness_match data_con = mkMatch
+        (mkPrefixFunRhs (L loc damlSerializableWitness_RDR))
+        [con_pat, nlVarPat z_rdr]
+        (foldr
+            (nlHsApp . nlHsApp witness . nlHsVar)
+            (nlHsVar z_rdr)
+            as_needed)
+        (noLoc emptyLocalBinds)
       where
-         data_con_RDR  = getRdrName data_con
-         con_arity     = dataConSourceArity data_con
-         as_needed     = take con_arity as_RDRs
-         con_pat       = nlConVarPat data_con_RDR as_needed
-         witness       = nlHsVar witness_rdr
+         data_con_RDR = getRdrName data_con
+         con_arity    = dataConSourceArity data_con
+         as_needed    = take con_arity as_rdrs
+         con_pat      = parenthesizePat appPrec $ nlConVarPat data_con_RDR as_needed
+         witness      = nlHsVar damlSerializableWitness_RDR
 
+    as_rdrs :: [RdrName]
+    as_rdrs = [ mkVarUnqual (mkFastString ("a"++show i)) | i <- [(1::Int) .. ] ]
     z_rdr = mkVarUnqual (mkFastString "z")
-
-    as_RDRs :: [RdrName]
-    as_RDRs = [ mkVarUnqual (mkFastString ("a"++show i)) | i <- [(1::Int) .. ] ]
