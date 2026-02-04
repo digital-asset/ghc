f420fb2d00850ec0dc1b064c5f0303cdfc811f27
Jasper Van der Jeugt <jasper.vanderjeugt@digitalasset.com>
2026-02-04 15:36:38 +0100

    Allow Serializable instances in daml-stdlib-0.0.0

--- compiler/typecheck/TcValidity.hs
+++ compiler/typecheck/TcValidity.hs
@@ -1404,7 +1404,8 @@ check_valid_inst_head dflags is_boot is_sig ctxt clas cls_args
   -- Rules for thee, not for me: we need some way to have prim type instances
   , thisPackage dflags /= primUnitId &&
       thisPackage dflags /= fsToUnitId (fsLit "daml-stdlib") &&
-      thisPackage dflags /= fsToUnitId (fsLit "daml-stdlib-2.2")
+      thisPackage dflags /= fsToUnitId (fsLit "daml-stdlib-2.2") &&
+      thisPackage dflags /= fsToUnitId (fsLit "daml-stdlib-0.0.0")
   = failWithTc $
     text "Class" <+> quotes (ppr clas_nm) <+>
     text "requires deriving(" <> ppr clas_nm <> text ") syntax"
